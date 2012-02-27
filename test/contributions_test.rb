7ea88ca3aceb56ce25926866958bcf7486d39a12
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-02-27 10:00:06 -0500

    major reorganization of Contributions tests
    This probably should've been more than one commit, but it got out of
    hand and there is no going back.  It's madness, I say!

--- test/contributions_test.rb
+++ test/contributions_test.rb
@@ -1,7 +1,11 @@
 require 'teststrap'
 require 'contributions'
 
-context "Contributions::Contributions" do
+context "Contributions::Contributions public api with a mocked .new" do
+  hookup do
+    stub(Contributions::GithubAPI).forks(anything) { ['r/r', 's/s'] }
+  end
+
   setup { Contributions::Contributions.new(:username => 'charlietanksley') }
 
   context ".new" do
@@ -10,107 +14,189 @@ context "Contributions::Contributions" do
       asserts_topic.assigns :repositories
     end
 
-    context "finds out about all forks" do
-      asserts(:repositories).includes "msanders/snipmate.vim"
-      asserts(:repositories).includes "thumblemonks/riot"
+    asserts(:repositories).equals ['r/r', 's/s']
+  end
+
+  context ".add will add repositories" do
+    context "if given a string" do
+      setup { topic.add 'added/added' }
+      asserts_topic.includes 'added/added'
+    end
+
+    context "if given an array" do
+      setup { topic.add ['added/first', 'added/second'] }
+      asserts_topic.includes 'added/first'
+      asserts_topic.includes 'added/second'
     end
   end
 
-  context "#contributions" do
-    setup do
-      Contributions::Contributions.new(:username => 'u')
+  context ".contributions_as_hash" do
+    context "does not reload hash if it already exists" do
     end
-
-    hookup do
-      mock(Contributions::GithubAPI).name('u') { 'Charlie Tanksley' }
-      mock(Contributions::Git).contributions('Charlie Tanksley', 'thumblemonks/riot') { 'no way' }
-    end
-
-    asserts(:contributions, 'thumblemonks/riot').equals 'no way'
   end
 
-  # context "#gather" do
-  #   setup { Contributions::Contributions.new(:username => 'u') }
+  context ".reload_contributions is an alias for load_contributions" do
+  end
 
-  #   context "asks for the forks, then determines the user's contributions for each" do
-  #     hookup do
-  #       mock(topic).forks { ['sinatra/sinatra'] }
-  #       mock(topic).contributions(anything).times(1) { Hash.new }
-  #     end
 
-  #     asserts(:gather).kind_of Hash
-  #   end
-  # end
 
-  # context "#forks" do
+  # context "manipulators" do
   #   setup { Contributions::Contributions.new(:username => 'vim-scripts') }
 
-  #   context "adds the result of a GithubAPI.forks to the repositories ivar" do
-  #     helper(:repos) { ['vim-scripts/test.zip'] }
-  #     hookup do
-  #       mock(Contributions::GithubAPI).forks('vim-scripts') { repos }
-  #     end
-  #     asserts(:forks).equals ['vim-scripts/test.zip']
+  #   context "#remove will remove a single repository" do
+  #     hookup { topic.repositories = ['r/r', 's/s', 't/t'] }
+  #     asserts(:remove, 's/s').equals ['r/r', 't/t']
+  #     asserts(:repositories).equals ['r/r', 't/t']
   #   end
+
+  #   context "#remove will remove an array of repositories" do
+  #     hookup { topic.repositories = ['r/r', 's/s', 't/t'] }
+  #     asserts(:remove, ['r/r', 't/t']).equals ['s/s']
+  #     asserts(:repositories).equals ['s/s']
+  #   end
+
+  #   context "#add will add a single repository" do
+  #     hookup { topic.repositories = ['r/r', 's/s'] }
+  #     asserts(:add, 't/t').equals ['r/r', 's/s', 't/t']
+  #     asserts(:repositories).equals ['r/r', 's/s', 't/t']
+  #   end
+
+  #   context "#add will add an array of repositories" do
+  #     hookup { topic.repositories = ['r/r'] }
+  #     asserts(:add, ['s/s', 't/t']).equals ['r/r', 's/s', 't/t']
+  #     asserts(:repositories).equals ['r/r', 's/s', 't/t']
+  #   end
+
+
   # end
 
-  context "manipulators" do
-    setup { Contributions::Contributions.new(:username => 'vim-scripts') }
+  # context "#contributions" do
+  #   setup do
+  #     Contributions::Contributions.new(:username => 'u')
+  #   end
 
-    context "#remove will remove a single repository" do
-      hookup { topic.repositories = ['r/r', 's/s', 't/t'] }
-      asserts(:remove, 's/s').equals ['r/r', 't/t']
-      asserts(:repositories).equals ['r/r', 't/t']
-    end
+  #   hookup do
+  #     mock(Contributions::GithubAPI).name('u') { 'Charlie Tanksley' }
+  #     mock(Contributions::Git).contributions('Charlie Tanksley', 'thumblemonks/riot') { 'no way' }
+  #   end
 
-    context "#remove will remove an array of repositories" do
-      hookup { topic.repositories = ['r/r', 's/s', 't/t'] }
-      asserts(:remove, ['r/r', 't/t']).equals ['s/s']
-      asserts(:repositories).equals ['s/s']
-    end
+  #   asserts(:contributions, 'thumblemonks/riot').equals 'no way'
+  # end
 
-    context "#add will add a single repository" do
-      hookup { topic.repositories = ['r/r', 's/s'] }
-      asserts(:add, 't/t').equals ['r/r', 's/s', 't/t']
-      asserts(:repositories).equals ['r/r', 's/s', 't/t']
-    end
+  # # context "#gather" do
+  # #   setup { Contributions::Contributions.new(:username => 'u') }
 
-    context "#add will add an array of repositories" do
-      hookup { topic.repositories = ['r/r'] }
-      asserts(:add, ['s/s', 't/t']).equals ['r/r', 's/s', 't/t']
-      asserts(:repositories).equals ['r/r', 's/s', 't/t']
-    end
+  # #   context "asks for the forks, then determines the user's contributions for each" do
+  # #     hookup do
+  # #       mock(topic).forks { ['sinatra/sinatra'] }
+  # #       mock(topic).contributions(anything).times(1) { Hash.new }
+  # #     end
+
+  # #     asserts(:gather).kind_of Hash
+  # #   end
+  # # end
+
+  # # context "#forks" do
+  # #   setup { Contributions::Contributions.new(:username => 'vim-scripts') }
+
+  # #   context "adds the result of a GithubAPI.forks to the repositories ivar" do
+  # #     helper(:repos) { ['vim-scripts/test.zip'] }
+  # #     hookup do
+  # #       mock(Contributions::GithubAPI).forks('vim-scripts') { repos }
+  # #     end
+  # #     asserts(:forks).equals ['vim-scripts/test.zip']
+  # #   end
+  # # end
 
 
+  # context "#update" do
+  #   context "changes nothing if there are no updates" do
+  #     setup { Contributions::Contributions.new(:username => 'vim-scripts') }
+  #     hookup { topic.repositories = ['s/s', 'h/h'] }
+
+  #     asserts(:update).equals ['s/s', 'h/h']
+  #   end
+
+  #   context "alters the array as per execpt and add" do
+  #     setup do
+  #       Contributions::Contributions.new(:username => 'vim-scripts',
+  #                                        :remove => ['h/h'],
+  #                                        :add => ['r/r'])
+  #     end
+  #     hookup { topic.repositories = ['s/s', 'h/h'] }
+
+  #     asserts(:update).equals ['s/s', 'r/r']
+  #   end
+
+  #   context "alters the array as per execpt and add" do
+  #     setup do
+  #       Contributions::Contributions.new(:username => 'vim-scripts',
+  #                                        :only => ['h/h'])
+  #     end
+  #     hookup { topic.repositories = ['s/s', 'z/z'] }
+
+  #     asserts(:update).equals ['h/h']
+  #   end
+  # end
+end
+
+
+context "Contributions::Contributions internal methods with a mocked .new" do
+  hookup do
+    mock(Contributions::GithubAPI).forks(anything) { ['r/r', 's/s'] }
   end
 
-  context "#update" do
-    context "changes nothing if there are no updates" do
-      setup { Contributions::Contributions.new(:username => 'vim-scripts') }
-      hookup { topic.repositories = ['s/s', 'h/h'] }
+  setup { Contributions::Contributions.new(:username => 'charlietanksley') }
 
-      asserts(:update).equals ['s/s', 'h/h']
+  context ".load_contributions" do
+    context "actually calls out to get the contributions for each fork" do
     end
 
-    context "alters the array as per execpt and add" do
-      setup do
-        Contributions::Contributions.new(:username => 'vim-scripts',
-                                         :remove => ['h/h'],
-                                         :add => ['r/r'])
-      end
-      hookup { topic.repositories = ['s/s', 'h/h'] }
-
-      asserts(:update).equals ['s/s', 'r/r']
+    context "returns a hash with repository names as keys" do
     end
 
-    context "alters the array as per execpt and add" do
-      setup do
-        Contributions::Contributions.new(:username => 'vim-scripts',
-                                         :only => ['h/h'])
-      end
-      hookup { topic.repositories = ['s/s', 'z/z'] }
-
-      asserts(:update).equals ['h/h']
+    context "stashes the results in an instance variable" do
     end
   end
 end
+
+context "Contributions::Contributions with unmocked .new" do
+  hookup { RR.reset }
+  helper(:full) { Contributions::Contributions.new(:username => 'charlietanksley') }
+
+  context ".new finds out about all forks" do
+    setup { full }
+    asserts(:repositories).includes "msanders/snipmate.vim"
+    asserts(:repositories).includes "thumblemonks/riot"
+  end
+
+  context ".new with an :only modifies the forks" do
+    setup { Contributions::Contributions.new(:username => 'charlietanksley', :only => 'thumblemonks/riot') }
+    asserts(:repositories).equals ['thumblemonks/riot']
+  end
+
+  context ".new with a :remove subtracts a fork" do
+    setup { Contributions::Contributions.new(:username => 'charlietanksley', :remove => 'thumblemonks/riot') }
+    denies(:repositories).includes 'thumblemonks/riot'
+    asserts("we have subtracted 1") { full.repositories.count - topic.repositories.count == 1 }.equals true
+  end
+
+  context ".new will :remove with an array as an argument" do
+    setup { Contributions::Contributions.new(:username => 'charlietanksley', :remove => ['thumblemonks/riot', 'rubinius/rubinius']) }
+    denies(:repositories).includes 'thumblemonks/riot'
+    denies(:repositories).includes 'rubinius/rubinius'
+  end
+
+  context ".new with an :add adds a fork" do
+    setup { Contributions::Contributions.new(:username => 'charlietanksley', :add => 'thumblemonks/chicago') }
+    asserts(:repositories).includes 'thumblemonks/chicago'
+    asserts("we have added 1") { topic.repositories.count - full.repositories.count == 1 }.equals true
+  end
+
+  context ".new will :add with an array as an argument" do
+    setup { Contributions::Contributions.new(:username => 'charlietanksley', :add => ['t/r', 'r/r']) }
+    asserts(:repositories).includes 't/r'
+    asserts(:repositories).includes 'r/r'
+  end
+end
+
