cc14da87462510dd8bcea047f16a70b08596742f
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-03-27 15:52:28 -0400

    refine integration tests to make them as efficient as I can
    cut nearly 20 seconds off the real time

--- test/contributions_integration_test.rb
+++ test/contributions_integration_test.rb
@@ -1,38 +1,46 @@
 require 'teststrap'
 require 'contributions'
 
-context "Contributions::Contributions" do
+context "a full-stack run of Contributions::Contributions" do
   hookup { RR.reset }
   setup { Contributions::Contributions.new(:username => 'charlietanksley') }
 
   context "starts with a list of repositories" do
-    asserts(:repositories).equals ['GreenvilleRB/greenvillerb.github.com', 'msanders/snipmate.vim']
+    setup { topic.repositories }
+    asserts_topic.includes 'GreenvilleRB/greenvillerb.github.com'
+    asserts_topic.includes 'msanders/snipmate.vim'
   end
 
   context "does not start with any contributions" do
     asserts(:contributions).nil
   end
 
-  context "grabs the contributions when asked" do
-
-    context "with the right keys" do
-      setup { topic.contributions_as_hash.keys }
-      asserts_topic.includes 'GreenvilleRB/greenvillerb.github.com'
-      denies_topic.includes 'msanders/snipmate.vim'
+  context "when looking at two repositories" do
+    hookup do
+      topic.only ['GreenvilleRB/greenvillerb.github.com',
+                        'msanders/snipmate.vim']
     end
 
-    context "and saves them off" do
-      hookup { topic.contributions_as_hash }
-      setup { topic.contributions.keys }
-      asserts_topic.includes 'GreenvilleRB/greenvillerb.github.com'
-      denies_topic.includes 'msanders/snipmate.vim'
-    end
-  end
+    context "grabs the contributions when asked" do
+      context "with the right keys" do
+        setup { topic.contributions_as_hash.keys }
+        asserts_topic.includes 'GreenvilleRB/greenvillerb.github.com'
+        denies_topic.includes 'msanders/snipmate.vim'
+      end
 
-  context "when the list is updated" do
-    hookup { topic.remove ['GreenvilleRB/greenvillerb.github.com'] }
-    hookup { topic.load_contributions }
-    asserts { topic.contributions_as_hash.keys }.equals []
-    asserts(:contributions_as_hash).equals Hash[]
+      context "and saves them off" do
+        hookup { topic.contributions_as_hash }
+        setup { topic.contributions.keys }
+        asserts_topic.includes 'GreenvilleRB/greenvillerb.github.com'
+        denies_topic.includes 'msanders/snipmate.vim'
+      end
+    end
+
+    context "you can force a reloading of the repositories" do
+      hookup { topic.remove ['GreenvilleRB/greenvillerb.github.com'] }
+      hookup { topic.load_contributions }
+      asserts { topic.contributions_as_hash.keys }.equals []
+      asserts(:contributions_as_hash).equals Hash[]
+    end
   end
 end
