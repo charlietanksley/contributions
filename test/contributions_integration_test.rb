3e68e6462768bf37af074c41d738cd5aa8214dae
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-03-27 15:42:50 -0400

    remove as many integration tests as possible
    These just aren't needed.  There is no reason to test these without
    stubs here.  The functionality that needs to be tested is tested with
    what remains (the part that actually interfaces with github).  Speed
    this up some.

--- test/contributions_integration_test.rb
+++ test/contributions_integration_test.rb
@@ -3,50 +3,10 @@ require 'contributions'
 
 context "Contributions::Contributions" do
   hookup { RR.reset }
-  helper(:full) { Contributions::Contributions.new(:username => 'charlietanksley') }
-
-  context ".new finds out about all forks" do
-    setup { full }
-    asserts(:repositories).includes "msanders/snipmate.vim"
-    asserts(:repositories).includes "thumblemonks/riot"
-  end
-
-  context ".new with an :only modifies the forks" do
-    setup { Contributions::Contributions.new(:username => 'charlietanksley', :only => 'thumblemonks/riot') }
-    asserts(:repositories).equals ['thumblemonks/riot']
-  end
-
-  context ".new with a :remove subtracts a fork" do
-    setup { Contributions::Contributions.new(:username => 'charlietanksley', :remove => 'thumblemonks/riot') }
-    denies(:repositories).includes 'thumblemonks/riot'
-    asserts("we have subtracted 1") { full.repositories.count - topic.repositories.count == 1 }.equals true
-  end
-
-  context ".new will :remove with an array as an argument" do
-    setup { Contributions::Contributions.new(:username => 'charlietanksley', :remove => ['thumblemonks/riot', 'rubinius/rubinius']) }
-    denies(:repositories).includes 'thumblemonks/riot'
-    denies(:repositories).includes 'rubinius/rubinius'
-  end
-
-  context ".new with an :add adds a fork" do
-    setup { Contributions::Contributions.new(:username => 'charlietanksley', :add => 'thumblemonks/chicago') }
-    asserts(:repositories).includes 'thumblemonks/chicago'
-    asserts("we have added 1") { topic.repositories.count - full.repositories.count == 1 }.equals true
-  end
-
-  context ".new will :add with an array as an argument" do
-    setup { Contributions::Contributions.new(:username => 'charlietanksley', :add => ['t/r', 'r/r']) }
-    asserts(:repositories).includes 't/r'
-    asserts(:repositories).includes 'r/r'
-  end
-end
-
-context "a full run of Contributions::Contributions" do
-  setup { Contributions::Contributions.new(:username => 'charlietanksley',
-                                           :only => ['thumblemonks/riot', 'msanders/snipmate.vim', 'davetron5000/methadone']) }
+  setup { Contributions::Contributions.new(:username => 'charlietanksley') }
 
   context "starts with a list of repositories" do
-    asserts(:repositories).equals ['thumblemonks/riot', 'msanders/snipmate.vim', 'davetron5000/methadone']
+    asserts(:repositories).equals ['GreenvilleRB/greenvillerb.github.com', 'msanders/snipmate.vim']
   end
 
   context "does not start with any contributions" do
@@ -57,20 +17,20 @@ context "a full run of Contributions::Contributions" do
 
     context "with the right keys" do
       setup { topic.contributions_as_hash.keys }
-      asserts_topic.includes 'thumblemonks/riot'
-      asserts_topic.includes 'davetron5000/methadone'
+      asserts_topic.includes 'GreenvilleRB/greenvillerb.github.com'
+      denies_topic.includes 'msanders/snipmate.vim'
     end
 
     context "and saves them off" do
       hookup { topic.contributions_as_hash }
       setup { topic.contributions.keys }
-      asserts_topic.includes 'thumblemonks/riot'
-      asserts_topic.includes 'davetron5000/methadone'
+      asserts_topic.includes 'GreenvilleRB/greenvillerb.github.com'
+      denies_topic.includes 'msanders/snipmate.vim'
     end
   end
 
   context "when the list is updated" do
-    hookup { topic.remove ['thumblemonks/riot', 'davetron5000/methadone'] }
+    hookup { topic.remove ['GreenvilleRB/greenvillerb.github.com'] }
     hookup { topic.load_contributions }
     asserts { topic.contributions_as_hash.keys }.equals []
     asserts(:contributions_as_hash).equals Hash[]
